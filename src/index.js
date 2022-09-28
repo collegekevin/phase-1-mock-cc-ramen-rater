84e1353c5c28a927b63710631f9885733191aa14
Kevin Price <collegekevin@gmail.com>
2022-09-28 10:29:58 -0700

    Oh boy.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,50 @@
 // write your code here
-// Change before adding/committing/pushing
+// I still have a ways to go to get this... 
+//hopefully I can make big strides in the next 22 hours.
+
+
+const fetchRamen = () => {
+    const url = 'http://localhost:3000/ramens'
+    fetch(url)
+    .then(resp => resp.json())
+    //.then(resp => console.log(json))
+    //Crap! Nothing was logged
+    .then(json => renderRamenToScreen(json))
+}
+
+const renderRamenToScreen = ramens => {
+    const ramenImage = document.getElementById('ramen-menu')
+    ramens.forEach(ramens => {
+        makeRamenCard(ramens)
+    })
+}
+
+const displayRamen = ramens => {
+
+
+
+}
+
+
+const makeRamenCard = ramen => {
+    const newRamenName = document.getElementById('new-name')
+    const newRamenRestaurant = document.createElement('new-restaurant')
+    const newRamenImage = document.createElement('new-image')
+    const newRamenRating = document.createElement('new-rating')
+    const newRamenComment = document.createElement('new-comment')
+
+    ramenName.textContent = ramen.name
+    ramenRestaurant.textConent = ramen.restaurant
+    ramenImage = ramen.image
+    ramenRating.textContent = ramen.rating
+    ramenComment.textContent = ramen.content
+
+
+}
+
+
+
+
+
+
+
